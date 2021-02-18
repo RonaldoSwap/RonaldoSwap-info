6aa9a5ddbf7362b8dcd23d3785a916591288fb45
saltKosher <saltswapfinance@gmail.com>
2021-02-18 17:19:51 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SaltSwap History
+
+Check it out live: [https://saltswap.info/](https://saltswap.info/).
+
+### To Start Development
+
+###### Installing dependencies
+
+```bash
+yarn
+```
+
+###### Running locally
+
+```bash
+yarn start
+```
---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/saltswap/saltswap-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://saltswap.finance" rel="noopener noreferrer" target="_blank">
+      saltswap
+    </a>{' '}
+    |{' '}
+    <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
